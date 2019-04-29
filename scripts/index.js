70c89e0754743755df9f8ede4fcacb6d41b6729d
Anjan Roy <anjanroy@yandex.com>
2019-04-29 23:11:05 +0530

    added responsiveness to website

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,32 @@
-let width = 5;
-document.addEventListener( // listens to dom event, when page is loaded completely, this callback gets invoked.
-    "DOMContentLoaded", () => {
-        setTimeout(() => { // invocation is delayed by 2000ms, on purpose
-            setInterval(startProgressBar, 1000); // then this function keeps invoking `startProgressBar` after each 1000ms.
-        }, 1000);
-    }
-);
-function startProgressBar() {
-    // extending width of progress bar, displaying div element.
-    width = (width + 5) % 100;
-    document.getElementById("div2").style.width = `${width}%`;
-}
-function iconOnClickHandler() {
-    window.open("https://github.com/itzmeanjan", "_blank");
-}
+'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+    let style = 'background: linear-gradient(to top right, #333333, white);';
+    setInterval(() => {
+        style = style === 'background: linear-gradient(to top right, #333333, white);' ? 'background: linear-gradient(to bottom right, #333333, white);' : 'background: linear-gradient(to top right, #333333, white);';
+        document.getElementById('parentDiv').style = style;
+    }, 10);
+    document.getElementById('myProfileDiv').addEventListener('mouseenter', () => {
+        document.getElementById('myProfileDiv').style = 'background: linear-gradient(to bottom right, #7763c0, rgb(85, 161, 167));';
+        document.getElementById('profileText1').style = 'color: white';
+        document.getElementById('profileText2').style='display: inline';
+    });
+    document.getElementById('myProfileDiv').addEventListener('mouseleave', () => {
+        document.getElementById('myProfileDiv').style = 'background: linear-gradient(to bottom right, black, white);';
+        document.getElementById('profileText1').style = 'color: black';
+        document.getElementById('profileText2').style='display: none';
+    });
+    document.getElementById('articleButton').addEventListener('click', () => {
+        document.getElementById('articleButton').style = 'background: violet;';
+        setTimeout(() => {
+            window.open('https://github.com/itzmeanjan?tab=repositories');
+            document.getElementById('articleButton').style = 'background: linear-gradient(to bottom right,#ddeeff, #11bb11);';
+        }, 100);
+    });
+    document.getElementById('projectButton').addEventListener('click', () => {
+        document.getElementById('projectButton').style = 'background: violet;';
+        setTimeout(() => {
+            window.open('https://dev.to/itzmeanjan');
+            document.getElementById('projectButton').style = 'background: linear-gradient(to bottom left,#ddeeff, #11bb11);';
+        }, 100);
+    });
+});
