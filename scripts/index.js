4ef958ab3b8fe0e33e4c59339592e53ff0bd1c4f
Anjan Roy <anjanroy@yandex.com>
2019-05-10 00:41:32 +0530

    removed image

--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('aboutImage').addEventListener('click', () => {
-        window.open('https://github.com/itzmeanjan', '_blank');
-    });
-});
